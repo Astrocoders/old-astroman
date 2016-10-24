c51291e14c813edac871d5ea507f79c7e75ad0c2
Gabriel Rubens <grsabreu@gmail.com>
2016-10-24 16:19:45 -0200

    Update package info

--- packages/easy-testing/package.js
+++ packages/easy-testing/package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'astrocoders:easy-testing',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'One-install Meteor full testing setup',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Astrocoders/meteor-easy-testing',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
