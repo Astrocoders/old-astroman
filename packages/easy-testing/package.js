4b79589fde0e70660a7e45bbc6e51d18594bf333
Gabriel Rubens <grsabreu@gmail.com>
2016-10-24 15:35:50 -0200

    First commit

---
+++ packages/easy-testing/package.js
@@ -0,0 +1,32 @@
+Package.describe({
+  name: 'astrocoders:easy-testing',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.1.2');
+  api.use('ecmascript');
+
+  api.imply([
+    'lmieulet:meteor-coverage@1.1.3',
+    'practicalmeteor:mocha@2.4.5_6',
+    'practicalmeteor:mocha-console-runner@0.2.1',
+    'dispatch:mocha-phantomjs@0.1.7',
+  ])
+
+  api.mainModule('easy-testing.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('astro:easy-testing');
+  api.mainModule('easy-testing-tests.js');
+});
