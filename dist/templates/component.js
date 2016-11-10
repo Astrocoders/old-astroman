f0f2129ac8c38ff9866efae8c3e67f1e9190c7cb
Gabriel Rubens <grsabreu@gmail.com>
2016-11-10 21:28:48 -0200

    Upldate component template

--- dist/templates/component.js
+++ dist/templates/component.js
@@ -6,5 +6,5 @@ Object.defineProperty(exports, "__esModule", {
 
 exports.default = function (_ref) {
   var name = _ref.name;
-  return "import React, {\n  Component,\n  PropTypes,\n} from 'react'\n\nexport default class " + name + " extends Component {\n  static propTypes = {\n  }\n\n  render(){\n    return (\n    )\n  }\n}\n";
+  return "import React, {\n  PropTypes,\n} from 'react'\n\nexport default function " + name + "(){\n  return (\n  )\n}\n\n" + name + ".propTypes = {\n}\n";
 };
