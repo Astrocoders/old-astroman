3038f275ca283ae58f46685b31788f3fb62b952a
Gabriel Rubens <grsabreu@gmail.com>
2017-01-17 18:25:18 -0300

    Untrack dist

--- dist/commands/init/react-native/react_native_relay.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = reactNativeMeteor;
-function reactNativeMeteor(_ref) {
-  var name = _ref.name;
-}
